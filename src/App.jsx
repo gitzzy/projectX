54790c712707cfbcb8b201ce961ec3770a600c9a
gitzzy <tyagidevansh10@gmail.com>
2025-09-21 10:41:53 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Menubar from './components/Menubar'
 import Navbar from './components/Navbar'
 import Base from './components/Base'
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
     {/* <Menubar/> */}
      {/* <Navbar/> */}
      <Base/>
+     <Analytics />
     </>
   )
 }
